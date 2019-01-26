468c63e62878cc03e1f656420e6928f959104b30
HungryJoe <sammkagan@gmail.com>
2019-01-26 16:33:55 -0600

    Created Sam's test scene, got basics working

---
+++ No. My Home Is Better/Assets/block_place.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block_place : MonoBehaviour
+{
+	public GameObject block;
+	public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(0)) {
+			Vector3 pos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+			pos[2] = 0;
+			Object.Instantiate(block, pos, Quaternion.identity);
+			Debug.Log(Input.mousePosition);
+	   }
+    }
+}
