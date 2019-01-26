622b372066db3e8b7aa1f4e9a04e1c5bd7e5ed1a
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2019-01-26 00:38:55 -0800

    Added Player Tester

---
+++ No. My Home Is Better/Assets/PlayerControlsTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlsTest : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private float xVal;
+    
+    [SerializeField]
+    private float height;
+
+    [SerializeField]
+    private Rigidbody2D playerBody;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float xVal = Input.GetAxis("Horizontal");
+        
+        playerBody.velocity = new Vector2(xVal * speed, playerBody.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerBody.velocity = new Vector2(playerBody.velocity.x, height);
+        }
+
+    }
+}
