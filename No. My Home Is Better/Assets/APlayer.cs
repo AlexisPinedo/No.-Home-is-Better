caac27ddc84f607cd0efbe5afcbc1cf9166593f0
HungryJoe <sammkagan@gmail.com>
2019-01-27 01:49:24 -0600

    Revert "Revert "Merge branch 'master' of github.com:AlexisPinedo/No.-Home-is-Better""
    This reverts commit aff02a1271f7809ba0943ede1c2c5e56d03cf546.

---
+++ No. My Home Is Better/Assets/APlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APlayer : MonoBehaviour
+{
+//    public PlayerInput Input { get; private set; }
+//    
+//    public PlayerScript Player { get; private set; }
+//    
+//    public int PlayerNumber { get; private set; }
+//    
+//    public void SetPlayer(PlayerScript player)
+//    {
+//        this.Player = Player;
+//        PlayerNumber = player.playerNumber;
+//        Input = player.GetComponent<PlayerInput>();
+//    }
+
+}
