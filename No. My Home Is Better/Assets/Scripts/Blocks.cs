970ba68d8af6c2e5ad71a88912dc36953f137fbc
shishir-reddy <shishir1@uci.edu>
2019-01-26 22:50:08 -0800

    Removed current cursor

--- No. My Home Is Better/Assets/Scripts/Blocks.cs
+++ No. My Home Is Better/Assets/Scripts/Blocks.cs
@@ -7,28 +7,13 @@ public class Blocks : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Instantiation of cursor blocks
+
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        ContactPoint2D [] contacts = new ContactPoint2D[20];
-        collision.GetContacts(contacts);
-        Debug.Log(collision.contacts);
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        foreach (var item in collision.contacts)
-        {
-            Debug.DrawLine(transform.position, collision.transform.position);
-
-        }
-    }
 }
