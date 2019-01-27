4b9f958daf3a16285a2a5691cd8a015aab48c74e
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2019-01-26 23:04:07 -0800

    Revert "Revert "no message""
    This reverts commit 2db58d7c14204145941bcecbb608a5da26c688ee.

---
+++ No. My Home Is Better/Assets/Scripts/PlayerPanel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+
+public class PlayerPanel : MonoBehaviour
+{
+   
+    private bool HasControllerAssigned;
+   
+    private int controllerNumber = 1;
+
+    //[SerializeField] 
+    //private GameObject Player;
+    
+    [SerializeField]
+    private PlayerInput Input;
+
+    private string PlayerTextBoxNumber;
+
+    private APlayer player;
+    
+    [SerializeField]
+    private Text Player1TextBox;
+
+    [SerializeField] private Canvas PlayerCanvas;
+
+    private void Awake()
+    {
+        HasControllerAssigned = false;
+    }
+
+
+    public void Update()
+    {
+        Debug.Log("Running");
+        if(UnityEngine.Input.GetButton("Jump") && !HasControllerAssigned)
+        {
+            Debug.Log("A button was pressed");
+            AssignController(controllerNumber);
+            controllerNumber++;
+        }
+
+        if (HasControllerAssigned && Input.ButtonIsDown(PlayerInput.Button.A))
+        {
+            Debug.Log("Setting panel off");
+            PlayerCanvas.gameObject.SetActive(false);
+        }
+            
+    }
+
+//    public bool isReady()
+//    {
+//        if (!HasControllerAssigned)
+//            return false;
+//        return true;
+//    }
+//    
+    public void AssignController(int controller)
+    {
+        Debug.Log("Setting player to controller");
+        Input.SetControllerNumber(controller);
+        HasControllerAssigned = true;
+        Player1TextBox.text = "Ready";
+    }
+}
