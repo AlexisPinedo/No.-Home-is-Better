157c082b5998ebf09cd67d3e2e91408483992bf6
shishir-reddy <shishir1@uci.edu>
2019-01-27 17:51:40 -0800

    bug fix/ art

--- No. My Home Is Better/Assets/Store.cs
+++ No. My Home Is Better/Assets/Store.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Store : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject block;
+    
 
     private GameObject playerBlock;
 
@@ -13,19 +12,6 @@ public class Store : MonoBehaviour
 
     private bool stay = false;
     
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.collider.CompareTag("Player"))
-        {
-            collidedPlayer = other.gameObject.GetComponent<Player>();
-            if (collidedPlayer.blockGrabbed == false)
-            {
-                Instantiate(block, new Vector3(other.transform.position.x + .5f, other.transform.position.y + 0.1f, other.transform.position.z), Quaternion.identity);
-                playerBlock = GameObject.Find("Block(Clone)");
-                playerBlock.transform.parent = collidedPlayer.transform;
-                collidedPlayer.blockGrabbed = true;
-            }
-        }
-    }
+    
 
 }
